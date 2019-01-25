2ad133eec6446cbb4423df40638cb78edff6e69d
Gary Bermudez <garybm.17@gmail.com>
2019-01-25 14:58:52 -0800

    add alert if the user does not enter a name

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,25 @@
 
 //This is the backend functionality of the website
 var beepBoop = function(userInput){
-  var results = [];
+var results = [];
+var nameInput = $("#name").val();
+  if(nameInput ==="") {
+    alert("Please enter your name.")
+  }
+
   for (var i = 0; i <= userInput; i++) {
     var inputString = (i).toString();
+
     if (i === 1) {
       results.push("Beep!");
     } else if (inputString.includes("2")) {
       results.push("Boop! ")
-  
+    } else if (inputString.includes("3")) {
+      results.push("I'm sorry  I'm afraid I can't do that. ");
+    } else {
+      results.push(""+ i +". ")
+    }
+  }
   return results;
 }
 
@@ -17,12 +28,13 @@ var beepBoop = function(userInput){
 
 $(document).ready(function(){
   $("#input-form").submit(function(event){
-    event.preventDefault();
     var initialInput = parseInt($("#user-input").val());
     var output = beepBoop(initialInput);
     var outputString = output.join("");
 
     $("#results").show();
     $("#results").text(outputString);
+      event.preventDefault();
+
   });
 });
