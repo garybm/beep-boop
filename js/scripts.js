9719eec8a6cf17a50da1ca11942d58e870cc2e5c
Gary Bermudez <garybm.17@gmail.com>
2019-01-25 10:44:43 -0800

    add form

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,28 @@
 	// ◦	Example input: 1
 	// ◦	Example output: "Beep!"
 
-function replaceOne(number) {
-  for (var i = 0; i <= number; i++) {
-  var numberList = (i.toString()).split("");
-    if (i === 0 && i !== 0) {
-      numberList.push("I'm sorry, " + name + ". I'm afraid I can't do that.")
-    }
-};
- return numberList;
-
-}
+// function replaceOne(number) {
+//   var results = [];
+//   for (var i = 0; i <= number; i++) {
+//   var inputString = (i.toString()).split("");
+//     if (i === 1 && i !== 0) {
+//       number.push("beep!");
+//     };
+// };
+//  return numberList;
+//
+// }
 
 
 
 
 // User Interface Logic
 $(document).ready(function() {
-  $("#beepBoop").submit(function(event) {
-    var number = parseInt($("input#number").val());
-      event.preventDefault();
+  $("#formOne").submit(function(event) {
+    var inputNumber =($("input#number").val());
+    $(".numberOne").text(inputNumber);
+    $("#story").show();
+
+    event.preventDefault();
   });
 });
